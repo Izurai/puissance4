b2b5ac90a9c198eab12d304e87c71e99939fae8a
Izurai <bill.gharbi@gmail.com>
2025-05-13 20:40:30 +0200

    Patatra

--- puissance4.ml
+++ puissance4.ml
@@ -3,15 +3,19 @@
 let hauteur_plateau = 6;;
 let largeur_plateau = 7;;
 
-let plateau = Array.make largeur_plateau (Array.make hauteur_plateau 0);;
+let plateau = Array.make largeur_plateau (Array.make hauteur_plateau "0");;
 
 (* Fonction pour afficher le plateau *)
+(* On parcours le tableau avec les deux boucles, on affiche un espace si la case
+ est vide et les caractères "x" ou "o" selon les pions dans les cases *)
 
 let afficher_plateau p =
         for i = 0 to hauteur_plateau - 1 do
                 for j = 0 to largeur_plateau - 1 do
-                        print_string("  ");
-                        print_int(p.(j).(i));
+                        print_string("||");
+                        if p.(j).(i) = "0" then
+                                print_string(" ")
+                        else print_str(p.(j).(i));
                 done;
                 print_newline();
         done;;
@@ -19,5 +23,3 @@ let afficher_plateau p =
 (* Fonction pour jouer un coup *)
 
 (* Fonction pour voir si un joueur à gagner *)
-
-afficher_plateau plateau;;
