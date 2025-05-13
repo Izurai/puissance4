f25317ea5660f797ef67151779f5638cf7b2d388
Izurai <bill.gharbi@gmail.com>
2025-05-13 21:18:11 +0200

    Maintenant on peut afficher un plateau de jeu

--- puissance4.ml
+++ puissance4.ml
@@ -1,21 +1,30 @@
+type plateau = {
+        p: string array array;
+        largeur: int;
+        hauteur: int;
+};;
+
 (* Fonction pour choisir la taille du plateau *)
 
-let hauteur_plateau = 6;;
-let largeur_plateau = 7;;
-
-let plateau = Array.make largeur_plateau (Array.make hauteur_plateau "0");;
+let hauteur_plateau = 6;; (* mettre un instream pour changer la valeur *)
+let largeur_plateau = 7;; (* ici aussi *)
+let plateau1 = {
+        p = Array.make largeur_plateau (Array.make hauteur_plateau "0");
+        hauteur = hauteur_plateau;
+        largeur = largeur_plateau;
+};;
 
 (* Fonction pour afficher le plateau *)
 (* On parcours le tableau avec les deux boucles, on affiche un espace si la case
  est vide et les caractères "x" ou "o" selon les pions dans les cases *)
 
-let afficher_plateau p =
-        for i = 0 to hauteur_plateau - 1 do
-                for j = 0 to largeur_plateau - 1 do
+let afficher_plateau (plateau:plateau) =
+        for i = 0 to plateau.hauteur - 1 do
+                for j = 0 to plateau.largeur - 1 do
                         print_string("||");
-                        if p.(j).(i) = "0" then
-                                print_string(" ")
-                        else print_str(p.(j).(i));
+                        if plateau.p.(j).(i) = "0" then (* voir la définition du type plateau, ici plateau.p désigne les cases (string array array) *)
+                                print_string("󰴈")
+                        else print_string(plateau.p.(j).(i));
                 done;
                 print_newline();
         done;;
@@ -23,3 +32,5 @@ let afficher_plateau p =
 (* Fonction pour jouer un coup *)
 
 (* Fonction pour voir si un joueur à gagner *)
+
+afficher_plateau plateau1
